4f077dd0e9a8add5ba603ee1c5a7f23b89245d2f
jonny <2248977158@qq.com>
2019-02-16 00:23:58 +0800

    info

--- src/Pages/PersonalInfo.js
+++ src/Pages/PersonalInfo.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { ImageBackground, Text, View, StyleSheet, ScrollView } from 'react-native';
+import { ImageBackground, Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
 import { Button, Flex, List, InputItem } from '@ant-design/react-native';
 import { appBg, theme } from '../Index';
 import { connect } from 'react-redux';
 
 const styles = StyleSheet.create({
 	container: {
-        flex: 1,
+        flex: 1
     },
 	backgroundImage:{
 	    flex:1,
@@ -14,15 +14,46 @@ const styles = StyleSheet.create({
 	    width:null,
 	    width:null,
 	    backgroundColor:'rgba(0,0,0,0)',
+	    paddingTop: 20
 	},
-	title: {
-		marginTop: theme.appTopHeight,
-		textAlign: 'center',
-		color: 'white',
-		fontSize: 20,
-		fontWeight: 'bold',
-		marginBottom: 30
-	}
+	text: {
+		fontSize: 18,
+		color: '#fff',
+		width: '30%',
+		textAlign: 'right',
+		paddingRight: 15,
+		paddingTop: 15,
+        paddingBottom: 15,
+		
+	},
+	confirmWrapper: {
+        width: 160,
+        marginTop: 15,
+        paddingTop: 15,
+        paddingBottom: 15,
+        borderRadius: 60,
+        backgroundColor: theme.opacityWhite,
+        flexDirection: 'row',
+    	justifyContent: 'center',
+    },
+    item: {
+    	marginBottom: 15,
+    },
+    confirm: {
+        fontSize: 18,
+        color: '#fff',
+    },
+    inputField: {
+        width: '60%',
+        fontSize: 16,
+        color: '#fff',
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
+        textAlign: 'center',
+        backgroundColor: theme.opacityWhite,
+    },
 });
 
 class PersonalInfo extends Component {
@@ -56,106 +87,137 @@ class PersonalInfo extends Component {
 	// }
 	render () {
         const {username, resident_id_card, alipay, bank_card, promotion_code} = this.props;
+        // const {username, resident_id_card, alipay, bank_card, promotion_code} = {username: 'zzz', resident_id_card: '23er2f', alipay: '1111', bank_card: '2222', promotion_code: '43324'};
 		return (
 			<View style={styles.container}>
 				<ImageBackground source={appBg} style={styles.backgroundImage}>
-					<Text style={styles.title}>个人信息</Text>
 					<ScrollView
 					>
-						<List>
-							<InputItem
-				            clear
-				            value={username}
-				            onChange={value => {
-				              this.setState({
-				                userName: value,
-				              });
-				            }}
-                            placeholder="请输入名字"
-                            editable={!username}
-				          	>
-				            	姓名
-				            </InputItem>
-
-				            <InputItem
-				            clear
-				            value={resident_id_card}
-				            onChange={value => {
-				              this.setState({
-				                idCard: value,
-				              });
-				            }}
-                            placeholder="请输入身份证"
-                            editable={!resident_id_card}
-				          	>
-				            	身份证
-				            </InputItem>
-
-				            <InputItem
-				            clear
-				            type="phone"
-				            value={alipay}
-				            onChange={value => {
-				              this.setState({
-				                phoneNumber: value,
-				              });
-				            }}
-                            placeholder="请输入手机号"
-                            editable={!alipay}
-				          	>
-				            	手机号
-				            </InputItem>
-				            <InputItem
-				            clear
-				            type="bankCard"
-				            value={bank_card}
-				            onChange={value => {
-				              this.setState({
-				                bankCard: value,
-				              });
-				            }}
-                            placeholder="请输入银行卡"
-                            editable={bank_card}
-				          	>
-				            	银行卡
-				            </InputItem>
-				            <InputItem
-				            clear
-				            value={alipay}
-				            onChange={value => {
-				              this.setState({
-				                aPay: value,
-				              });
-				            }}
-                            placeholder="请输入支付宝"
-                            editable={alipay}
-				          	>
-				            	支付宝
-				            </InputItem>
-				            <InputItem
-				            clear
-				            value={promotion_code}
-				            onChange={value => {
-				              this.setState({
-				                spreadCode: value,
-				              });
-				            }}
-                            placeholder="请输入推广码"
-                            editable={promotion_code}
-				          	>
-				            	推广码
-				            </InputItem>
-				            <List.Item>
-					            <Button
-					              onPress={() => {
-
-					              }}
-					              type="primary"
-					            >
-					              确定
-					            </Button>
-					        </List.Item>
-						</List>
+						<View>
+							<Flex
+							style={styles.item}
+							align="center"
+							>
+								<Text style={styles.text}>姓名</Text>
+								<TextInput
+								style={styles.inputField}
+							    defaultValue={username}
+							    onChangeText={(value) => {
+							      this.setState({
+							        userName: value,
+							      });
+							    }}
+							    placeholder="请输入名字"
+							    editable={(username == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<View>
+							<Flex
+							align="center"
+							style={styles.item}
+							>
+								<Text style={styles.text}>身份证</Text>
+								<TextInput
+								style={styles.inputField}
+							    defaultValue={resident_id_card}
+							    onChangeText={(value) => {
+							      this.setState({
+							        idCard: value,
+							      });
+							    }}
+							    placeholder="请输身份证"
+							    editable={(resident_id_card == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<View>
+							<Flex
+							style={styles.item}
+							align="center"
+							>
+								<Text style={styles.text}>手机号</Text>
+								<TextInput
+								style={styles.inputField}
+							    defaultValue={alipay}
+							    maxLength={11}
+							    keyboardType="numeric"
+							    onChangeText={(value) => {
+							      const newPhone = money.replace(/[^\d]+/, '');
+							      this.setState({
+							        phoneNumber: newPhone,
+							      });
+							    }}
+							    placeholder="请输入手机号"
+							    editable={(alipay == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<View>
+							<Flex
+							style={styles.item}
+							align="center"
+							>
+								<Text style={styles.text}>银行卡号</Text>
+								<TextInput
+								style={styles.inputField}
+							    defaultValue={bank_card}
+							    onChangeText={(value) => {
+							      this.setState({
+							        bankCard: value,
+							      });
+							    }}
+							    placeholder="请输入银行卡号"
+							    editable={(bank_card == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<View>
+							<Flex
+							style={styles.item}
+							align="center"
+							>
+								<Text style={styles.text}>支付宝</Text>
+								<TextInput
+								style={styles.inputField}
+							    defaultValue={alipay}
+							    onChangeText={(value) => {
+							      this.setState({
+							        aPay: value,
+							      });
+							    }}
+							    placeholder="请输入支付宝号"
+							    editable={(alipay == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<View>
+							<Flex
+							style={styles.item}
+							align="center"
+							>
+								<Text style={styles.text}>推广码</Text>
+								<TextInput 
+								style={styles.inputField}
+							    defaultValue={promotion_code}
+							    onChangeText={(value) => {
+							      this.setState({
+							        spreadCode: value,
+							      });
+							    }}
+							    placeholder="请输入推广码"
+							    editable={(promotion_code == '') ? true : false}
+								/>
+							</Flex>
+						</View>
+						<Flex
+						align="center"
+						justify="center"
+						>
+							<View style={styles.confirmWrapper}>
+	                            <Text style={styles.confirm}>确定</Text>
+	                        </View>
+                        </Flex>
 					</ScrollView>
 			    </ImageBackground>
 			</View>
